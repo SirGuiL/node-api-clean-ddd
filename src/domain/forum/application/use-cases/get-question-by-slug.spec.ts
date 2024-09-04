18eaadff39900a31a970fa2a4ff2f7ab1b69836a
SirGuiL <guibiel-10@hotmail.com>
2024-09-04 14:51:56 -0300

    feat: finished search by slug use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,32 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-question-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Question } from '../../enterprise/entities/question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get question by slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = await Question.create({
+      authorId: new UniqueEntityId(),
+      title: 'Example question',
+      slug: Slug.create('example-question'),
+      content: 'Example content',
+    })
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+  })
+})
